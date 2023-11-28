7126c77374305a0ddc7039462c835328a2903e5d
Axa Uribe <uri19002@byui.edu>
2023-11-28 10:22:40 -0700

    Download-Button

--- src/lib/Board.svelte
+++ src/lib/Board.svelte
@@ -49,6 +49,12 @@
     }
   };
 
+  // Favorite Function Code
+  var btnvar1 = document.getElementById("btn1"); 
+
+  // function Toggle1(){
+  //   if (btnvar1.style.color )
+  // }
 </script>
 
 <div>
@@ -57,9 +63,14 @@
   <div class="board-container">
     {#each imageUrls as imageUrl, i (imageUrl)}
       <div class="board-item">
+        <!-- Download Function -->
         <button class="download-button" on:click={() => downloadImage(imageUrl)}>
           <img src="./src/images/Download-Icon.jpg" alt="download">
         </button>
+        <!-- Favorites Funtion -->
+        <button on:click={() => Toggle1()} class="btn" id="btn1"><img src="./src/images/unmarked-favorites.png" alt="btn1"></button>
+        <button on:click={() => Toggle2()} class="btn" id="btn2"><img src="./src/images/marked-favorites.png" alt="btn2"></button>
+        <!-- Fetching Images -->
         <img src={imageUrl} alt={imageAlts[i] || "Unsplash"} />
         <h3>Artist: {imageArtists[i]}</h3>
       </div>
@@ -113,6 +124,28 @@
 
     .download-button:hover img {
       transform: scale(1.2);
+    }
+
+    /* Favorite Function */
+    .btn{
+      background: none;
+      border: 0; 
+    }
+
+    .btn:hover img {
+      transform: scale(1.2);
+    }
+
+    .btn img {
+      width: 4.2rem;
+    }
+
+    .btn {
+      position: absolute;
+      top: 0;
+      right: 0.5;
+      z-index: 1;
+      border: 0;
     }
 
   </style>
