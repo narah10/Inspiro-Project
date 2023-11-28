56109f76cf861381d642cd0f9b6ac8269e9daea8
Axa Uribe <uri19002@byui.edu>
2023-11-28 11:15:02 -0700

    Download Component

--- src/lib/Board.svelte
+++ src/lib/Board.svelte
@@ -1,10 +1,10 @@
 <script>
-    import { saveAs } from 'file-saver';
     export let imageUrls = [];
     export let imageAlts = [];
     export let imageArtists = [];
+    import  DownloadDetails  from "./downloadDetails.svelte";
 
-    async function boardData() {
+    export async function boardData() {
       const url = "https://api.unsplash.com/";
       const endpoint = "photos";
       const access_key = "IftTCZlrrtO-pbVD1lRZWSppEas03FUG7ahRjmFwXag";
@@ -47,23 +47,8 @@
       }
     });
   } 
-  
-   // Download Button
-    const downloadImage = async (imageUrl) => {
-      try {
-        const response = await fetch(imageUrl);
 
-        // Assuming the response is an image, you can use blob() to create a Blob object.
-        const imageBlob = await response.blob();
-
-        // Trigger the download using FileSaver.js
-        saveAs(imageBlob, "image.jpg");
-      } catch (error) {
-        console.error('Error downloading image:', error);
-      }
-    };
-   
-  </script>
+</script>
   
 <div>
   <h2>Welcome to Inspiro</h2>
@@ -71,83 +56,32 @@
   <div class="board-container">
     {#each imageUrls as imageUrl, i (imageUrl)}
       <div class="board-item">
-        <!-- Download Function -->
-        <button class="download-button" on:click={() => downloadImage(imageUrl)}>
-          <img src="./src/images/Download-Icon.jpg" alt="download">
-        </button>
         <!-- Fetching Images -->
         <img src={imageUrl} alt={imageAlts[i] || "Unsplash"} />
         <h3>Artist: {imageArtists[i]}</h3>
+        <DownloadDetails imageUrl = {imageUrl}/>
       </div>
     {/each}
   </div>
 </div>
 
 
-  <style>
-    .board-container {
-    column-count: 3;
-    column-gap: 15px;
-    }
-    .board-item {
-    display: inline-block;
-    width: 100%;
-    }
-    .board-item img {
-    display:block;
-    width: 100%;
-    }
-
-    /* Download Button Set Up */
-    .board-item {
-      position: relative; /* Make the .board-item container a positioning context */
-    }
-
-    .board-item .download-button{
-      background-color: transparent;
-    }
-
-    .download-button {
-      position: absolute;
-      top: 0;
-      right: 0;
-      z-index: 1;
-      border: 0;
-    }
-
-    .download-button:hover {
-      transition: transform 0.5s ease-in-out;
-    }
-
-    .download-button img {
-      width: 4.5rem;
-    }
-
-    .download-button:hover img {
-      transform: scale(1.2);
-    }
-
-    /* Favorite Function */
-    .btn{
-      background: none;
-      border: 0; 
-    }
-
-    .btn:hover img {
-      transform: scale(1.2);
-    }
-
-    .btn img {
-      width: 4.2rem;
-    }
-
-    .btn {
-      position: absolute;
-      top: 0;
-      right: 0.5;
-      z-index: 1;
-      border: 0;
-    }
-
-  </style>
+<style>
+  .board-item {
+    position: relative; 
+  }
+  
+  .board-container {
+  column-count: 3;
+  column-gap: 15px;
+  }
+  .board-item {
+  display: inline-block;
+  width: 100%;
+  }
+  .board-item img {
+  display:block;
+  width: 100%;
+  }
+</style>
   
