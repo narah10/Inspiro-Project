b10be036466801b9db11c79f23ca3f7d25c5e61f
Mitzi Vite <vit20001@byui.edu>
2023-12-05 12:03:46 -0700

    Login pop up

--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -1,60 +1,70 @@
 <script>
-    import { signUp, login } from "./auth.mjs";
-    // import { userStore } from "./stores.mjs";
-    let email = "";
-    let password = "";
-    let loading = false;
-    let errorMessage = "";
-  
-    async function handleSignup(e) {
-      try {
-        loading = true;
-        // signup
-        signUp({ email, password });
-        // if fails throw error
-      } catch (err) {
-        errorMessage = err;
-      } finally {
-        loading = false;
-      }
+  import { signUp, login } from "./auth.mjs";
+  import Swal from "sweetalert2";
+  import canvasConfetti from "canvas-confetti";
+
+  let email = "";
+  let password = "";
+  let loading = false;
+  let errorMessage = "";
+
+  async function handleSignup(e) {
+    try {
+      loading = true;
+      signUp({ email, password });
+
+    } catch (err) {
+      errorMessage = err;
+    } finally {
+      loading = false;
     }
-    async function handleLogin(e) {
-      e.preventDefault();
-      try {
-        loading = true;
-        // login
-        login({ email, password });
-  
-        // if fails throw error
-      } catch (err) {
-        errorMessage = err;
-      } finally {
-        loading = false;
+  }
+
+  async function handleLogin(e) {
+    e.preventDefault();
+    try {
+      loading = true;
+
+      login({ email, password });
+
+ 
+      const loginSuccess = true;
+
+      if (loginSuccess) {
+
+        await Swal.fire({
+          icon: 'success',
+          title: 'Login Successful!',
+          showConfirmButton: false,
+          timer: 1500,
+        });
+
+        // Trigger confetti effect
+        const confettiSettings = { target: 'login-confetti' };
+        canvasConfetti(confettiSettings);
       }
+    } catch (err) {
+      errorMessage = err;
+    } finally {
+      loading = false;
     }
-  </script>
-  
-  <p>Sign in Below</p>
-  <form on:submit={handleLogin}>
-    <p>
-      <label for="email">Email</label>
-      <input type="text" id="email" name="email" bind:value={email} />
-    </p>
-    <p>
-      <label for="password">Password</label>
-      <input
-        type="password"
-        name="password"
-        id="password"
-        bind:value={password}
-      />
-    </p>
-    {#if errorMessage}
-      <p>{errorMessage}</p>
-    {/if}
-    <button disabled={loading}>Login</button>
-    <button type="button" disabled={loading} on:click={handleSignup}
-      >Signup</button
-    >
-  </form>
-  
+  }
+</script>
+
+<p>Sign in Below</p>
+<form on:submit={handleLogin}>
+  <p>
+    <label for="email">Email</label>
+    <input type="text" id="email" name="email" bind:value={email} />
+  </p>
+  <p>
+    <label for="password">Password</label>
+    <input type="password" name="password" id="password" bind:value={password} />
+  </p>
+  {#if errorMessage}
+    <p>{errorMessage}</p>
+  {/if}
+  <button disabled={loading}>Login</button>
+  <button type="button" disabled={loading} on:click={handleSignup}>Signup</button>
+</form>
+<div id="login-confetti"></div>
