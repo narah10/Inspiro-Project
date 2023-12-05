e32ea6666ca834885c8b1fae121ad79d6c859fb4
Lisa <lisacjheinhold@gmail.com>
2023-12-05 15:42:33 -0700

    styled login

--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -51,20 +51,50 @@
   }
 </script>
 
-<p>Sign in Below</p>
 <form on:submit={handleLogin}>
-  <p>
+  <div>
     <label for="email">Email</label>
     <input type="text" id="email" name="email" bind:value={email} />
-  </p>
-  <p>
+  </div>
+  <div>
     <label for="password">Password</label>
     <input type="password" name="password" id="password" bind:value={password} />
-  </p>
+  </div>
   {#if errorMessage}
+  <div>
     <p>{errorMessage}</p>
+  </div>
   {/if}
   <button disabled={loading}>Login</button>
   <button type="button" disabled={loading} on:click={handleSignup}>Signup</button>
 </form>
 <div id="login-confetti"></div>
+
+<style>
+  div {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 1em;
+  }
+  input, label{
+    display: block;
+    text-align: left;
+    font-size: 1.1em;
+  }
+  input {
+     padding: 0.5em;
+     border: 1px solid #ccc;
+     border-radius: 5px;
+     margin-bottom: 1em;
+     font-size: 1.1em;
+ }
+ button {
+       padding: 0.5em;
+       border: 1px solid #ccc;
+       color: white;
+       border-radius: 5px;
+       margin-bottom: 1em;
+       font-size: 1.1em;
+       background-color: #5A55BC;
+ }
+</style>
