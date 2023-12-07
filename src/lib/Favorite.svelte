6fc22707d1893a4bb6f9702095169fcdc27500a0
narah10 <kwa18001@byui.edu>
2023-12-07 11:32:32 -0700

    Updated the favorite function as well as the bug for refreshing

--- src/lib/Favorite.svelte
+++ src/lib/Favorite.svelte
@@ -1,5 +1,7 @@
 <script>
       import { onMount } from 'svelte';
+      import DownloadDetails from './downloadDetails.svelte';
+      import FavoriteFunction from './favoriteFunction.svelte';
 
     let dataArray = [];
 
@@ -15,16 +17,37 @@
 
 <div>
     <h2>Favorites</h2>
-    <p>Browse</p>
-    <div>
+    <p>Browse your Images</p>
+    <div class="board-container">
         {#if dataArray.length > 0}
-          <ul>
-            {#each dataArray as item (item.id)}
+            {#each dataArray as item}
+            <div class="board-item">
             <img src={item} alt="Source from Unsplash"/>
-            {/each}
-          </ul>
+          </div>
+          {/each}
         {:else}
-          <p>No data in local storage</p>
+          <p>No Favorited Image</p>
         {/if}
       </div>
 </div>
+
+<style>
+.board-item {
+  position: relative; 
+}
+
+.board-container {
+column-count: 3;
+column-gap: 15px;
+}
+
+.board-item {
+display: inline-block;
+width: 100%;
+}
+
+.board-item img {
+display:block;
+width: 100%;
+}
+</style>
--- src/lib/stores.mjs
+++ src/lib/stores.mjs
@@ -1,5 +1,5 @@
 import { writable } from "svelte/store";
-export const route = writable("#home");
+export const route = writable(window.location.hash);
 export const userStore = writable({ isLoggedIn: false, user: null });
 export const currentImage = writable(null);
 export const favorites = writable(
