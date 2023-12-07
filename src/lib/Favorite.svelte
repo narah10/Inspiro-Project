9c54fb7d04e09a04d8d706dedfbd97733af4d9a8
narah10 <kwa18001@byui.edu>
2023-12-06 21:50:10 -0700

    Updated functionality for favorite board

--- src/lib/Favorite.svelte
+++ src/lib/Favorite.svelte
@@ -1,4 +1,30 @@
+<script>
+      import { onMount } from 'svelte';
+
+    let dataArray = [];
+
+    // Fetch the array from local storage on component mount
+    onMount(() => {
+    const storedData = localStorage.getItem('favorites');
+    if (storedData) {
+        dataArray = JSON.parse(storedData);
+        console.log(dataArray)
+    }
+    });
+</script>
+
 <div>
     <h2>Favorites</h2>
     <p>Browse</p>
+    <div>
+        {#if dataArray.length > 0}
+          <ul>
+            {#each dataArray as item (item.id)}
+            <img src={item} alt="Source from Unsplash"/>
+            {/each}
+          </ul>
+        {:else}
+          <p>No data in local storage</p>
+        {/if}
+      </div>
 </div>
